29d96af066a8779f7e1e17323ca28b0c5888240f
indieronin <indieronin101@gmail.com>
2021-05-20 21:43:16 +0200

    Base and Turret Update

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -5,6 +5,12 @@ public class MainMenu : Control
 {
     public void OnStartButtonUp()
     {
+        //Send the message for the game to show the save load screen
+        ChangeUIEvent cuie = new ChangeUIEvent();
+        cuie.callerClass = "MainMenu - OnSaveLoadButtonUp()";
+        cuie.showMenu = MENUS.NONE;
+        cuie.FireEvent();
+        
         //Send the message for the game start callback event with the load game set to false
         ChangeGameStateEvent cgse = new ChangeGameStateEvent();
         cgse.callerClass = "MainMenu - OnStartButtonUp()";
