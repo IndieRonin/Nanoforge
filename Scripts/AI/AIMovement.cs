7c231191fcc95720c3dd9c200f7ed3cd38b4da03
indieronin <indieronin101@gmail.com>
2021-06-14 15:42:50 +0200

    AIMovement Update

--- Scripts/AI/AIMovement.cs
+++ Scripts/AI/AIMovement.cs
@@ -45,12 +45,12 @@ public class AIMovement : Node
     // Called every physics frame. 'delta' is the elapsed time since the previous frame.
     public override void _PhysicsProcess(float delta)
     {
-        float newFollowForce = 0.0f;
-        //As the AI aprouches the target the target follow force is reduced
-        if (((Node2D)GetParent()).GlobalPosition.DistanceTo(targetPos) > 200.0f) newFollowForce = ;
+        //The distance to the target
+        float distanceToTarget = ((Node2D)GetParent()).GlobalPosition.DistanceTo(targetPos);
+        //The distance the ship will stop at and change state to attacking
+        float stopDistance = 200.0f;
         //Set the vector for the target folowing
         Vector2 targetVector = ((Node2D)GetParent()).GlobalPosition.DirectionTo(targetPos) * maxSpeed * targetFollowForce;
-
         //Update the boids forces
         UpdateBoids();
         //Apply the forces to the calculated vectors
@@ -59,9 +59,11 @@ public class AIMovement : Node
         seperationVector *= seperationForce;
         //Reset the acceleration to zero as to not acumulate the align and cohesion values over time
         acceleration = cohesionVector + alignVector + seperationVector + targetVector;
-
+        GD.Print("AIMovement - _PhysycsProcess(): acceleration = " + acceleration);
         //Set the velocity according ot the acceleration
         velocity = (velocity + acceleration).Clamped(maxSpeed);
+        GD.Print("AIMovement - _PhysycsProcess(): velocity = " + velocity);
+        if (distanceToTarget < stopDistance) velocity.LinearInterpolate(Vector2.Zero, 0.5f);
         //Look at the deisred direction
         ((Node2D)GetParent()).LookAt(velocity);
         //Set the position according to the velocity
