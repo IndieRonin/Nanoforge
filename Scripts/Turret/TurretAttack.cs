5445ecdad69322a3bf760941a7f33f1f811951ab
IndieRonin <indieronin@gmail.com>
2021-07-28 20:53:53 +0200

    Turret Attack Update

--- Scripts/Turret/TurretAttack.cs
+++ Scripts/Turret/TurretAttack.cs
@@ -9,8 +9,10 @@ public class TurretAttack : Node
     [Export] List<PackedScene> weaponsScenes = new List<PackedScene>();
     //The nodes for the initialized weapons
     List<Node2D> weapons = new List<Node2D>();
-    //The target the beam needs to be drawn toward
-    Node2D target;
+    //The list of targets node to track
+    List<Node2D> targets = new List<Node2D>();
+    //The speed the turret can turn at
+    [Export] float turnSpeed = 0.1f;
     //Function called at the creation of the object
     public override void _Ready()
     {
@@ -34,19 +36,69 @@ public class TurretAttack : Node
     // Called every physics frame. 'delta' is the elapsed time since the previous frame.
     public override void _PhysicsProcess(float delta)
     {
-        //If we don;t have a target we return out of the function without doing enything
-        if (target == null) return;
-        //If hte distance to the target is greater than the weapons range
-        if (((Node2D)GetParent()).GlobalPosition.DistanceTo(target.GlobalPosition) > 512.0f)
+        // //If we don;t have a target we return out of the function without doing enything
+        // if (target == null) return;
+        // //If hte distance to the target is greater than the weapons range
+        // if (((Node2D)GetParent()).GlobalPosition.DistanceTo(target.GlobalPosition) > 512.0f)
+        // {
+        //     //SSend the message tothe AI state manager to switch states
+        //     ChangeAIStateEvent caise = new ChangeAIStateEvent();
+        //     caise.callerClass = "AIAttack - _PhysicsProcess()";
+        //     caise.aiID = GetParent().GetInstanceId();
+        //     caise.newState = AIState.MOVE;
+        //     caise.FireEvent();
+        //     //Stop the physics process of the AIAttack before switching to the movement state
+        //     SetPhysicsProcess(false);
+        // }
+
+        //The tracking event for the turret =====================================================================================
+        //Get the vector direction to the target
+        Vector2 dir = targets[0].GlobalPosition - ((Node2D)GetParent()).GlobalPosition;
+        //Normalize the vector direction
+        dir = dir.Normalized();
+        //The angle to the target in radians
+        float newAngle = Mathf.Atan2(dir.y, dir.x);
+        //Set the rotation of hte parent object to the lerped angle of the new angle
+        ((Node2D)GetParent()).Rotation = Mathf.LerpAngle(((Node2D)GetParent()).Rotation, newAngle, turnSpeed);
+        // ======================================================================================================================
+    }
+    //When any area2d enters the line of sight of the turret it is registered and handled
+    public void OnLineOfSightAreaEntered(Area2D area)
+    {
+        //If the area is in the AI ship group
+        if (area.IsInGroup("AIShip"))
         {
-            //SSend the message tothe AI state manager to switch states
-            ChangeAIStateEvent caise = new ChangeAIStateEvent();
-            caise.callerClass = "AIAttack - _PhysicsProcess()";
-            caise.aiID = GetParent().GetInstanceId();
-            caise.newState = AIState.MOVE;
-            caise.FireEvent();
-            //Stop the physics process of the AIAttack before switching to the movement state
-            SetPhysicsProcess(false);
+            //If the tergats list is empty we just add the new target
+            if (targets.Count <= 0)
+            {
+                //Add the target tot the list of targets
+                targets.Add(area.GetParent() as Node2D);
+            }
+            else
+            {
+                //If the list already contains the target that entered the area  we just exit out of the function without doing anything
+                if (targets.Contains((Node2D)area.GetParent())) return;
+                //Add the target tot the list of targets
+                targets.Add(area.GetParent() as Node2D);
+            }
+            //Start the physics process of the turret track
+            SetPhysicsProcess(true);
+        }
+    }
+    //When any area2d exits the line of sight of the turret it is registered and handled
+    public void OnLineOfSightAreaExited(Area2D area)
+    {
+        if (area.IsInGroup("AIShip"))
+        {
+            //If the list already contains the target that entered the area  we just exit out of the function without doing anything
+            if (targets.Contains((Node2D)area.GetParent()))
+            {
+                //Remove the target from the list of targets
+                targets.Remove(area.GetParent() as Node2D);
+                //Stop the physics process of the turret AI if the targets list is empty
+                if (targets.Count == 0) SetPhysicsProcess(false);
+
+            }
         }
     }
     //Called when the SetAIAttackEvent message is recieved
